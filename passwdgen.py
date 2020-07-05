5bd04ee90d2b5ed0849925d21c598c2d5bcfc9e4
František Hruška <franta.hruska2013@gmail.com>
2020-07-06 01:04:46 +0200

    Update

--- passwdgen.py
+++ passwdgen.py
@@ -25,27 +25,27 @@ class MainApplication(tk.Frame):
 
         self.lbl_1.pack(fill="both")
 
-        self.btn2 = tk.Button(parent, text="Generate", command=self.update_password, bg='lightgreen')
-        self.btn2.pack(side="bottom", fill='x')
+        self.btn3 = tk.Button(parent, text='Exit', command=parent.destroy, bg='lightgreen')
+        self.btn3.pack(side="bottom", fill='x')
 
         self.btn1 = tk.Button(parent)
         self.btn1.config(text="Copy", bg='lightgreen')
         self.btn1.pack(side="bottom", fill='x')
 
-        self.btn3 = tk.Button(parent, text='Exit', command=parent.destroy, bg='lightgreen')
-        self.btn3.pack(side="bottom", fill='x')
+        self.btn2 = tk.Button(parent, text="Generate", command=self.update_password, bg='lightgreen')
+        self.btn2.pack(side="bottom", fill='x')
 
     def update_password(self):
-        password_local = self.password_gen()
-        self.lbl_1.config(text=password_local)
-        self.btn1.config(command=ppc.copy(password_local))
+        password = self.password_gen()
+        self.lbl_1.config(text=password)
+        self.btn1.config(command=ppc.copy(password))
         self.lbl_1.pack(fill="both")
 
     @staticmethod
     def password_gen():
         password_str = ''
         chars = list(st.ascii_letters + st.digits)
-        #chars = list(st.printable)
+        # chars = list(st.printable)
 
         for i in range(20):
             password_str += chars[random.randint(0, len(chars) - 1)]
