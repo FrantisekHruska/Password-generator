b5f9ce1d624f8c6b448b3e57ffcee54f816a4b4a
František Hruška <franta.hruska2013@gmail.com>
2020-08-23 12:29:00 +0200

    Added icon

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ class MainWindow(tk.Tk):
         self.geometry('620x80')
         self.resizable(False, False)
         self.config(bg='white')
+        self.iconbitmap('./img/key.ico')
         self.title("Password generator")
 
 
@@ -18,11 +19,11 @@ class MainApplication(tk.Frame):
 
     def __init__(self, parent, *args, **kwargs):
         super(MainApplication, self).__init__()
-        # self.parent = parent
+
         self.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.config(bg='white')
 
-        self.passlength = tk.IntVar(parent, value=20)
+        self.password_length = tk.IntVar(parent, value=20)
 
         self.difficulty_chars = tk.BooleanVar(parent, value=True)
         self.difficulty_numbers = tk.BooleanVar(parent, value=True)
@@ -52,29 +53,31 @@ class MainApplication(tk.Frame):
         self.checkbox_3 = tk.Checkbutton(parent, text="Punctuation", variable=self.difficulty_punctuation)
         self.checkbox_3.pack(side=tk.LEFT)
 
-        self.lbl_2 = tk.Label(parent, textvariable=self.passlength, width=2, height=2).pack(side=tk.LEFT)
+        self.lbl_2 = tk.Label(parent, textvariable=self.password_length, width=2, height=2).pack(side=tk.RIGHT)
         # self.lbl2 = tk.Label(parent, text="Length :").pack(side="right")
 
         self.slider_1 = tk.Scale(parent, from_=5, to=25, orient=tk.HORIZONTAL, length=85, width=20,
-                                 variable=self.passlength, sliderlength=10, bd=0, showvalue=False).pack(side=tk.RIGHT)
+                                 variable=self.password_length, sliderlength=10, bd=0, showvalue=False).pack(side=tk.RIGHT)
+        # self.update_password()
 
     def update_password(self):
-        diffstr = ''
-
-        if self.difficulty_chars.get():
-            diffstr += st.ascii_letters
-        if self.difficulty_numbers.get():
-            diffstr += st.digits
-        if self.difficulty_punctuation.get():
-            diffstr += st.punctuation
-
         try:
-            password = self.password_gen(list(diffstr), int(self.passlength.get()))
+            password = self.password_gen(self.set_difficulty(), int(self.password_length.get()))
             self.lbl_1.config(text=password)
             self.btn_1.config(command=ppc.copy(password))
         except ValueError:
             self.difficulty_chars.set(True)
-            messagebox.showerror(title='Error', message="Check at least one checkbox")
+            messagebox.showerror(title='Error', message="Tick at least one checkbox")
+
+    def set_difficulty(self):
+        password_difficulty = ''
+        if self.difficulty_chars.get():
+            password_difficulty += st.ascii_letters
+        if self.difficulty_numbers.get():
+            password_difficulty += st.digits
+        if self.difficulty_punctuation.get():
+            password_difficulty += st.punctuation
+        return list(password_difficulty)
 
     @staticmethod
     def password_gen(chars, length):
